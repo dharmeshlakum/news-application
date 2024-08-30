b2157a5f578f06342835cf4c5f37cca389589fac
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-08-30 14:19:17 +0530

    implement dark mode

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mode: "light"
+    }
+  }
+
+  handleDarkMode = () => {
+    if (this.state.mode === "light") {
+      this.setState({
+        mode: "dark"
+      });
+      document.body.style.backgroundColor = "#71797E"
+
+    } else {
+      this.setState({
+        mode: "light"
+      });
+      document.body.style.backgroundColor = "#ffffff"
+    }
+  }
+
   render() {
     return (
       <>
-        <Navbar heading={"News Daily"}/>
+        <Navbar
+          heading={"News Daily"}
+          mode={this.state.mode}
+          handleDarkMode={this.handleDarkMode} />
       </>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,57 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
 
     render() {
+        const { mode, handleDarkMode } = this.props
         return (
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav
+                className={`navbar navbar-expand-lg bg-${mode === "light" ? "light" : "dark"} text-${mode === "dark" ? "light" : "dark"}`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">{this.props.heading}</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <a
+                        className={`navbar-brand text-${mode === "dark" ? "light" : "dark"}`}
+                        href="/">
+                        {this.props.heading}
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a
+                                    className={`nav-link text-${mode === "dark" ? "light" : "dark"}`}
+                                    aria-current="page"
+                                    href="/">
+                                    Home
+                                </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">About</a>
+                                <a
+                                    className={`nav-link text-${mode === "dark" ? "light" : "dark"}`}
+                                    href="/">
+                                    About
+                                </a>
                             </li>
                         </ul>
                     </div>
                     <div className="form-check form-switch">
-                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" />
-                            <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Dark Mode</label>
+                        <input
+                            className="form-check-input"
+                            type="checkbox" role="switch"
+                            id="darkMode-check-box"
+                            onClick={handleDarkMode} />
+                        <label
+                            className="form-check-label"
+                            htmlFor="flexSwitchCheckChecked">
+                            Dark Mode
+                        </label>
                     </div>
                 </div>
             </nav>
